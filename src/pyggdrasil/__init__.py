729a4e08d5cfd912c6b344a38ce575d2eeaad1c9
Paweł Czyż <pczyz@protonmail.com>
2023-03-09 20:05:34 +0100

    Remove redundant imports.

--- src/pyggdrasil/__init__.py
+++ src/pyggdrasil/__init__.py
@@ -1,7 +1,4 @@
 """PYggdrasil: Python module for tree inference and analysis."""
 from pyggdrasil.tree import TreeNode
-from pyggdrasil.interface import TreeDistance
 
-import pyggdrasil.tree_inference as tree_inference
-
-__all__ = ["TreeNode", "TreeDistance", "tree_inference"]
+__all__ = ["TreeNode"]
