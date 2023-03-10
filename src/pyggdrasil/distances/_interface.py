bab1d3f94b0cd6008c3409368908de51935486fa
Paweł Czyż <pczyz@protonmail.com>
2023-03-10 12:25:06 +0100

    Add property for triangle inequality.

--- src/pyggdrasil/distances/_interface.py
+++ src/pyggdrasil/distances/_interface.py
@@ -13,6 +13,11 @@ class TreeDistance(Protocol):
     The hyperparameters of the metric should be set
     at the class initialization stage,
     similarly as with models in SciKit-Learn.
+
+    Note:
+        The distances between trees should be treated as tree
+        dissimilarity measures, rather than mathematical metrics.
+        For example, the triangle inequality does not need to hold.
     """
 
     def calculate_distance(
@@ -30,5 +35,26 @@ class TreeDistance(Protocol):
         raise NotImplementedError
 
     def is_symmetric(self) -> bool:
-        """Returns true if the distance function is symmetric."""
+        """Returns ``True`` if the distance function is symmetric,
+        i.e., :math:`d(t_1, t_2) = d(t_2, t_1)` for all pairs of trees.
+
+        Note:
+            If it is not known whether the distance function is symmetric,
+            ``False`` should be returned.
+        """
         return True
+
+    def triangle_inequality(self) -> bool:
+        """Returns ``True`` if the triangle inequality
+
+        .. math::
+
+           d(t_1, t_3) <= d(t_1, t_2) + d(t_2, t_3)
+
+        is known to hold for this distance.
+
+        Note:
+            If it is not known whether the triangle inequality
+            holds for a metric, ``False`` should be returned.
+        """
+        return False
