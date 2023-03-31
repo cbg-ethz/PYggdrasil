c7acd6f6beafd96245838855595a20ee09ac3377
gordonkoehn <gordon@koehn.net>
2023-03-31 09:25:48 +0200

    refine test_swap_node_labels_move

--- tests/tree_inference/test_mcmc.py
+++ tests/tree_inference/test_mcmc.py
@@ -1,5 +1,6 @@
 """Tests of the MCMC functions."""
 # _mcmc.py
+import pytest
 import jax.random as random
 import jax.numpy as jnp
 
@@ -8,13 +9,23 @@ import pyggdrasil.tree_inference._mcmc as mcmc
 import pyggdrasil.tree_inference as tree_inf
 
 
-def test_swap_node_labels_move():
+@pytest.mark.parametrize("seed", [42, 43, 44])
+def test_swap_node_labels_move(seed: int):
     """Test swap_node_labels."""
-    rng = random.PRNGKey(42)
-    adj_mat = tree_inf.generate_random_tree(rng, 10)
-    tree01 = mcmc.Tree(adj_mat, jnp.arange(10))
+    # get random numbers keys
+    rng = random.PRNGKey(seed)
+    rng_tree, rng_nodes = random.split(rng, 2)
+    # generate random tree
+    n_nodes = 10
+    adj_mat = tree_inf.generate_random_tree(rng_tree, n_nodes)
+    # generate random nodes
+    node1, node2 = random.randint(rng_nodes, shape=(2,), minval=0, maxval=n_nodes)
+    # assign labels
+    tree01 = mcmc.Tree(adj_mat, jnp.arange(n_nodes))
+    # swap labels
     tree01_labels = tree01.labels
-    tree02 = mcmc._swap_node_labels_move(tree01, 4, 8)
+    tree02 = mcmc._swap_node_labels_move(tree01, node1, node2)
     tree02_labels = tree02.labels
-    assert tree01_labels[4] == tree02_labels[8]
-    assert tree01_labels[8] == tree02_labels[4]
+    # check that labels are swapped
+    assert tree01_labels[node1] == tree02_labels[node2]
+    assert tree01_labels[node2] == tree02_labels[node1]
