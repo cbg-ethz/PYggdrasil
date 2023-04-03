ee1de728531414559cd8baf34463389e955082c9
gordonkoehn <gordon@koehn.net>
2023-04-03 14:42:25 +0200

    more efficient adj mat exponentiation

--- src/pyggdrasil/tree_inference/_mcmc_util.py
+++ src/pyggdrasil/tree_inference/_mcmc_util.py
@@ -26,12 +26,15 @@ def _get_descendants(adj_matrix: Array, labels: Array, parent: int) -> Array:
     diag_idx = jnp.diag_indices_from(adj_matrix)
     adj_matrix = adj_matrix.at[diag_idx].set(1)
     # boolean matrix exponentiation
-    bool_mat = jnp.where(adj_matrix == 1, True, False)
 
-    def body(carry, _):
-        return jnp.dot(carry, bool_mat), None
+    adj_matrix_exp = _expon_adj_mat(adj_matrix, n - 1)
 
-    adj_matrix_exp = jax.lax.scan(body, bool_mat, jnp.arange(n - 1))[0]
+    # bool_mat = jnp.where(adj_matrix == 1, True, False)
+    #
+    # def body(carry, _):
+    #     return jnp.dot(carry, bool_mat), None
+    #
+    # adj_matrix_exp = jax.lax.scan(body, bool_mat, jnp.arange(n - 1))[0]
     # get descendants
     desc = jnp.where(adj_matrix_exp[parent, :])[0]
     # get labels
@@ -39,6 +42,46 @@ def _get_descendants(adj_matrix: Array, labels: Array, parent: int) -> Array:
     # remove parent - as self-looped
     desc_labels = desc_labels[desc_labels != parent]
     return desc_labels
+
+
+def _expon_adj_mat(adj_matrix: Array, exp: int, cond: bool = False):
+    """Exponentiation of adjacency matrix.
+
+    Args:
+        adj_matrix : adjacency matrix
+        exp: exponent
+        cond: if to use conditional exponentiation,
+                stop if matrix does not change anymore
+    """
+    bool_mat = jnp.where(adj_matrix == 1, True, False)
+    adj_matrix_exp = bool_mat
+    if not cond:
+
+        def body(carry, _):
+            return jnp.dot(carry, bool_mat), None
+
+        adj_matrix_exp = jax.lax.scan(body, bool_mat, jnp.arange(exp))[0]
+    elif cond:
+        exp_counter = 0
+
+        def loop_cond_fn(carry):
+            prev_matrix, curr_matrix = carry
+            nonlocal exp_counter
+            exp_counter = exp_counter + 1
+            return ~(jnp.array_equal(prev_matrix, curr_matrix)) or (exp_counter <= exp)
+
+        def loop_body_fn(carry):
+            prev_matrix, curr_matrix = carry
+            new_matrix = jnp.dot(curr_matrix, bool_mat)
+            return curr_matrix, new_matrix
+
+        (_, adj_matrix_exp), _ = jax.lax.while_loop(
+            loop_cond_fn,
+            loop_body_fn,
+            (jnp.zeros_like(bool_mat), jnp.zeros_like(bool_mat)),
+        )
+
+    return adj_matrix_exp
 
 
 def _prune(tree: Tree, parent: int) -> tuple[Tree, Tree]:
--- tests/tree_inference/test_mcmc_util.py
+++ tests/tree_inference/test_mcmc_util.py
@@ -13,25 +13,22 @@ import pyggdrasil.tree_inference as tree_inf
 
 
 @pytest.mark.parametrize("seed", [42, 43, 44])
-def test_get_descendants(seed: int):
+@pytest.mark.parametrize("n_nodes", [5, 10, 15])
+def test_get_descendants(seed: int, n_nodes: int):
     """Test get_descendants against Floyd-Warshall algorithm."""
     # get random numbers keys
     rng = random.PRNGKey(seed)
     rng_tree, rng_nodes = random.split(rng, 2)
     # generate random tree
-    n_nodes = 10
     adj_mat = tree_inf.generate_random_tree(rng_tree, n_nodes)
     # generate random nodes
     parent = int(random.randint(rng_nodes, shape=(1,), minval=0, maxval=n_nodes)[0])
     # assign labels
     tree = mcmc.Tree(adj_mat, jnp.arange(n_nodes))
-    # get descendants
-
-    # TODO: fix this
+    # get descendants with matrix exponentiation
     desc01 = mcmc_util._get_descendants(
         jnp.array(tree.tree_topology), jnp.array(tree.labels), parent
     )
-
     # get descendants with Floyd-Warshall
     adj_mat_prime = np.array(tree.tree_topology)
     np.fill_diagonal(adj_mat_prime, 1)
