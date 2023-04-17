f94056ff9017816b2827fb47188c08b7544f0c85
gordonkoehn <gordon@koehn.net>
2023-04-17 14:09:40 +0200

    create file

---
+++ src/pyggdrasil/tree_inference/_logprob.py
@@ -0,0 +1,13 @@
+"""Implementation of the log-probability functions.
+
+The log-probability functions are used to calculate the log-probability of a tree.
+"""
+
+
+def logprobability_fn():
+    """Returns a function that calculates the log-probability of a tree.
+
+    Returns:
+        function that takes a tree and returns it's log-probability
+    """
+    raise NotImplementedError("Not implemented yet.")
