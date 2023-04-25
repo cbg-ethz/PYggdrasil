ebb8aecdfeaae2c68b67fc931fd6b1e4f4723b18
gordonkoehn <gordon@koehn.net>
2023-04-25 13:23:15 +0200

    WIP switching to networkX

--- src/pyggdrasil/visualize/tree.py
+++ src/pyggdrasil/visualize/tree.py
@@ -3,9 +3,15 @@
 from pathlib import Path
 import jax.numpy as jnp
 from anytree.exporter import DotExporter
+import pydot
 import os
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_pydot import graphviz_layout
+
+import networkx as nx
 
 from pyggdrasil.tree_inference._tree import Tree
+
 
 from pyggdrasil import TreeNode
 
@@ -36,7 +42,7 @@ def plot(tree: TreeNode, save_name: str, save_dir: Path, print_options: dict) ->
         None
     """
 
-    tree.print_topo()
+    # tree.print_topo()
 
     print(print_options)
 
@@ -44,16 +50,53 @@ def plot(tree: TreeNode, save_name: str, save_dir: Path, print_options: dict) ->
     # make output directory if it doesn't exist
     os.makedirs(save_dir, exist_ok=True)
 
-    # Assuming 'tree' is an instance of the TreeNode class
+    # DotExporter(tree).to_dotfile(fullpath + ".dot")
 
-    # DotExporter(tree, graph_attr=graph_attr).to_picture(fullpath + ".svg")
+    dot_string = ""
+    for line in DotExporter(tree):
+        dot_string += line
 
-    DotExporter(tree).to_dotfile(fullpath + ".dot")
+    graphs = pydot.graph_from_dot_data(dot_string)
+    graph = graphs[0]
 
-    # dotTree.graph_attr['label'] = 'My Graph Label'
-    # Add the label argument to the graph
+    nx_graph = nx.nx_pydot.from_pydot(graph)
 
-    # dotTree.render(fullpath + ".svg", view=True)  # Render the graph as a SVG file
+    fig = plt.figure()
+    pos = graphviz_layout(nx_graph, prog="dot")
+    ax1 = fig.add_subplot()
+    fig.suptitle("Maximum Likelihood Tree", fontsize=15)
+    nx.draw(
+        nx_graph,
+        with_labels=True,
+        ax=ax1,
+        pos=pos,
+        node_color="w",
+        edgecolors="k",
+        node_size=1000,
+        font_size=20,
+        font_weight="bold",
+    )
+    plt.text(0.9, 0.9, "log_prob =-5", dict(size=15), transform=ax1.transAxes)
+    plt.savefig(fullpath + ".svg", bbox_inches="tight")
+
+    # Parse the DOT string into a Graphviz Source object
+    # source = graphviz.Source(dot_string)
+
+    # Set graph attributes
+    # gv_graph.graph_attr(label='My Graph Title')
+    # gv_graph.graph_attr['rankdir'] = 'TB'
+
+    # Create a subgraph for the text
+    # with graph.subgraph() as sub:
+    #      # Set subgraph attributes
+    #      sub.attr(rank='same')
+    #      sub.attr(label='')
+    #
+    #      # Add text to the subgraph
+    #      my_var = 'log_prob'  # your variable containing the text
+    #      sub.node('text', label=my_var, shape='plaintext', pos='r', fontsize='20')
+
+    # gv_graph.render(fullpath + ".svg")  # Render the graph as a SVG file
 
 
 ################################################################################
@@ -72,4 +115,4 @@ if __name__ == "__main__":
     print_options["data_node"] = dict()
     # empty dict means print nothing - of node data
 
-    plot(root, "test02", Path("../../../data/trees/"), print_options)
+    plot(root, "test09", Path("../../../data/trees/"), print_options)
