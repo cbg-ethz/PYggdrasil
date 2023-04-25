bf033ce299f9850e565d8cb484869da428424df0
Gordon Julian Koehn <gordon@koehn.net>
2023-04-25 09:47:25 +0200

    Log Likelihood of a Tree (#17)
    * create file
    
    * WIP add single likelihood
    
    * WIP _mutation_likelihood
    
    * WIP add _attachment_prior
    
    * WIP frist draft, excluding muation_lik. attach..prior
    
    * WIP data type errors
    
    * remove unsused statement
    
    * Add comment on Ancestor Matrix
    
    * WIP _compute_mutation_likelihood + cleanup
    
    * untested _compute_mutation_likelihood
    
    * add test compute_likelihood
    
    * add log_mutation_likelihood
    
    * WIP log_prob
    
    * add tests for log_probability
    
    * typo
    
    * added 2D test of mutation likelihood computation
    
    * variable renaming for conventions

---
+++ src/pyggdrasil/tree_inference/_logprob.py
@@ -0,0 +1,141 @@
+"""Implementation of the log-probability functions.
+
+The log-probability functions are used to calculate the log-probability of a tree.
+"""
+import jax
+import jax.numpy as jnp
+import jax.scipy as jsp
+
+import pyggdrasil.tree_inference._tree as tr
+from pyggdrasil.tree_inference._tree import Tree
+
+from pyggdrasil.tree_inference._interface import MutationMatrix
+from pyggdrasil.tree_inference._interface import AncestorMatrix
+
+# Array of floats of shape (n, m, n+1) where n is the number of mutations,
+# m is the number of cells, and n+1 is the number of nodes in the tree.
+# The axis are (i=mutation, j=cell, k=attachment node)
+MutationLikelihood = jax.Array
+
+
+def logprobability_fn(
+    data: MutationMatrix, tree: tr.Tree, theta: tuple[float, float]
+) -> float:
+    """Returns a function that calculates the log-probability of a tree.
+
+    Args:
+        data: observed mutation matrix to calculate the log-probability of
+        tree: tree to calculate the log-probability of
+        theta: \theta = (\alpha, \beta) error rates
+
+    Returns:
+        log-probability of the tree
+        Implements the log-probability function from the paper: Equation 13
+        P(D|T,\theta) = \frac{T,\theta | D}{P(T,\theta)}
+    """
+
+    #  get log P(D_{ij} | A(T)_i~~sigma_j) i.e. the log mutation likelihood
+    log_mutation_likelihood = _log_mutation_likelihood(tree, data, theta)
+
+    # sum_{i=1}^{n} of prior
+    lse_arg = jnp.einsum("ijk->jk", log_mutation_likelihood)
+
+    # log sum_{k=1}^{n+1} exp or prior
+    # log sum exp operation over sigma_j = k
+    log_sum_exp = jsp.special.logsumexp(lse_arg, axis=-1)
+
+    # sum_{j=1}^{m}
+    log_prob = float(jnp.einsum("j->", log_sum_exp))
+
+    return log_prob
+
+
+def _log_mutation_likelihood(
+    tree: Tree,
+    mutation_mat: MutationMatrix,
+    theta: tuple[float, float],
+) -> MutationLikelihood:
+    """Returns the log-likelihood of a cell / mutation /attachment.
+
+    Args:
+        cell: cell index
+        mutation: mutation index
+        sigma: mutation node the cell is attached to
+        tree: tree object contains the tree topology, labels
+        mutation_mat: mutation matrix
+        theta: \theta = (\alpha, \beta) error rates
+
+    Returns:
+        likelihood of the cell / mutation - see Equation 13
+        log (P(D_{ij} | A(T)_{i˜sigma_j}) )
+            i / axis 0 has n dimensions sum over mutation nodes (n)
+                - each mutation
+            j / axis 1 has m dimensions sum over cells (m)
+                - each cell
+            k / axis 2 has n+1 dimensions sum over nodes (n+1)
+                - attachment to mutation and root
+    """
+
+    # A(T) - get ancestor matrix
+    ancestor_mat = tr._get_ancestor_matrix(tree.tree_topology)
+    # get mutation likelihood
+    mutation_likelihood = _mutation_likelihood(mutation_mat, ancestor_mat, theta)
+    # get log of it
+    log_mutation_likelihood = jnp.log(mutation_likelihood)
+
+    return log_mutation_likelihood
+
+
+def _mutation_likelihood(
+    mutation_matrix: MutationMatrix,
+    ancestor_matrix: AncestorMatrix,
+    theta: tuple[float, float],
+) -> MutationLikelihood:
+    """Returns the mutation likelihood given the data and expected mutation matrix.
+
+    Currently, only implements non-homozygous mutations.
+
+    Args:
+        mutation_matrix: mutation matrix
+        ancestor_matrix: ancestor matrix
+    Returns:
+        mutation likelihood - P(D_{ij} | A(T)_{i˜sigma_j})
+            i / axis 0 has n dimensions sum over mutation nodes (n)
+                - each mutation
+            j / axis 1 has m dimensions sum over cells (m)
+                - each cell
+            k / axis 2 has n+1 dimensions sum over nodes (n+1)
+                - attachment to mutation and root
+    Note:
+        let k = sigma_j
+    """
+
+    # m = number of cells, n-1 = number of mutations, ex root
+    n, m = mutation_matrix.shape
+    alpha, beta = theta
+
+    # truncate ancestor matrix
+    ancestor_matrix = ancestor_matrix[:-1]
+
+    # repeat the ancestor matrix  - tensor of dimensions (n, m, n+1)
+    ancestor_tensor = jnp.repeat(ancestor_matrix[:, jnp.newaxis, :], m, axis=1)
+
+    # repeat the mutation matrix  - tensor of dimensions (n, m, n+1)
+    mutation_tensor = jnp.repeat(mutation_matrix[:, :, jnp.newaxis], n + 1, axis=2)
+
+    # compute the likelihood
+    mutation_likelihood = jnp.zeros((n, m, n + 1))
+    # P(D_{ij} = 0| E_{ik} = 0) = 1- alpha
+    mask = (mutation_tensor == 0) & (ancestor_tensor == 0)
+    mutation_likelihood = jnp.where(mask, 1 - alpha, mutation_likelihood)
+    # P(D_{ij} = 0| E_{ik} = 1) = beta
+    mask = (mutation_tensor == 0) & (ancestor_tensor == 1)
+    mutation_likelihood = jnp.where(mask, beta, mutation_likelihood)
+    # P(D_{ij} = 1| E_{ik} = 0) = alpha
+    mask = (mutation_tensor == 1) & (ancestor_tensor == 0)
+    mutation_likelihood = jnp.where(mask, alpha, mutation_likelihood)
+    # P(D_{ij} = 1| E_{ik} = 1) = 1 - beta
+    mask = (mutation_tensor == 1) & (ancestor_tensor == 1)
+    mutation_likelihood = jnp.where(mask, 1 - beta, mutation_likelihood)
+
+    return mutation_likelihood
---
+++ tests/tree_inference/test_logprob.py
@@ -0,0 +1,194 @@
+"""Tests of the log-probability calculations."""
+# _logprob.py
+
+import jax.numpy as jnp
+import pytest
+
+import pyggdrasil.tree_inference._logprob as logprob
+from pyggdrasil.tree_inference._tree import Tree
+import pyggdrasil.tree_inference._tree as tr
+
+
+def test_mutation_likelihood_man():
+    """Test _compute_mutation_likelihood manually
+
+    Computes mutation likelihood given the true mutation matrix as data.
+    Sets error rates to alpha to 1 and also beta to 1 each, to check for
+    the correct total probability.
+    """
+
+    # define tree
+    adj_mat = jnp.array([[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0]])
+    labels = jnp.array([0, 1, 2, 3])
+    tree = Tree(adj_mat, labels)
+    ancestor_matrix = tr._get_ancestor_matrix(tree.tree_topology)
+    ancestor_matrix_truth = jnp.array(
+        [[1, 0, 0, 0], [1, 1, 0, 0], [0, 0, 1, 0], [1, 1, 1, 1]]
+    )
+
+    assert jnp.all(ancestor_matrix == ancestor_matrix_truth)
+
+    # cell attachment vector
+    # sigma = jnp.array([3, 2, 1 ,0, 1])
+    # get mutation matrix / data - get true mutation matrix
+    mutation_matrix = jnp.array([[0, 0, 0, 1, 0], [0, 0, 1, 1, 1], [0, 1, 0, 0, 0]])
+
+    # test for is mutation
+    # define error rates to check outcome
+    alpha = 1.0
+    beta = 0.0
+    theta = (alpha, beta)
+    # thus we expect only for data=1 and expected mutation=1 matrix probability of 1
+    # we expect 5 * n (=4) = 4 * j(=5) = 20 entries of unity in the final matrix
+
+    # define mutation likelihoods
+    # mutation_likelihoods = logprob._compute_mutation_likelihood(tree, ancestor_matrix)
+    mutation_likelihood = logprob._mutation_likelihood(
+        mutation_matrix, ancestor_matrix, theta
+    )
+
+    # check shape
+    n = 4
+    m = 5
+    assert mutation_likelihood.shape == (n - 1, m, n)
+
+    print(mutation_likelihood[:, :, 0])
+
+    # check total prob.
+    total_sum = jnp.einsum("ijk->", mutation_likelihood)
+    assert int(total_sum) == 20
+
+    # test for is not mutation
+    # define error rates to check outcome
+    alpha = 0.0
+    beta = 1.0
+    theta = (alpha, beta)
+    # thus we expect only for data=0 and expected mutation=0 matrix probability of 1
+    # we expect 10 * n (=4) = 8 * j(=5) = 40 entries of unity in the final matrix
+
+    # define mutation likelihoods
+    # mutation_likelihoods = logprob._compute_mutation_likelihood(tree, ancestor_matrix)
+    mutation_likelihood = logprob._mutation_likelihood(
+        mutation_matrix, ancestor_matrix, theta
+    )
+
+    # check shape
+    n = 4
+    m = 5
+    assert mutation_likelihood.shape == (n - 1, m, n)
+
+    print(mutation_likelihood[:, :, 0])
+
+    # check total prob.
+    total_sum = jnp.einsum("ijk->", mutation_likelihood)
+    assert jnp.equal(total_sum, 40)
+
+
+@pytest.mark.parametrize("alpha,", [0.1, 0.3])
+@pytest.mark.parametrize("beta,", [0.2, 0.4])
+def test_mutation_likelihood_2D(alpha, beta):
+    """Test _mutation_likelihood using a 2D example.
+    Checks for expected elements in the matrix."""
+
+    adj_mat = jnp.array([[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0]])
+    labels = jnp.array([0, 1, 2, 3])
+    tree = Tree(adj_mat, labels)
+
+    # cell j=0 attached to node 1
+    data_true = jnp.array([[0], [1], [0]])
+    # cell j=1 carries mutations different parts of tree - impossible given tree
+    data_false = jnp.array([[1], [0], [1]])
+
+    # expected mutation likelihoods - true
+    p_true = jnp.array(
+        [
+            [beta, 1 - alpha, 1 - alpha, 1 - alpha],
+            [1 - beta, 1 - beta, alpha, alpha],
+            [1 - alpha, 1 - alpha, beta, 1 - alpha],
+        ]
+    )
+    # expected mutation likelihoods - false
+    p_false = jnp.array(
+        [
+            [1 - beta, alpha, alpha, alpha],
+            [beta, beta, 1 - alpha, 1 - alpha],
+            [alpha, alpha, 1 - beta, alpha],
+        ]
+    )
+
+    p_mat_true = get_mutation_likelihood(tree, data_true, (alpha, beta))
+    p_mat_false = get_mutation_likelihood(tree, data_false, (alpha, beta))
+
+    assert jnp.all(p_mat_true == p_true)
+    assert jnp.all(p_mat_false == p_false)
+    assert jnp.einsum("ik->", p_mat_true) > jnp.einsum("ik->", p_mat_false)
+
+
+def get_mutation_likelihood(tree, mutation_mat, theta):
+    """Get mutation likelihood for a given tree and data - in 2D"""
+
+    # A(T) - get ancestor matrix
+    ancestor_mat = tr._get_ancestor_matrix(tree.tree_topology)
+    # get mutation likelihood
+    mutation_likelihood = logprob._mutation_likelihood(
+        mutation_mat, ancestor_mat, theta
+    )
+
+    # kill 3rd dimension
+    mutation_likelihood = jnp.einsum("ijk->ik", mutation_likelihood)
+
+    return mutation_likelihood
+
+
+def test_logprobability_fn():
+    """Test logprobability_fn manually."""
+    # define tree
+    adj_mat = jnp.array([[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0]])
+    labels = jnp.array([0, 1, 2, 3])
+    tree = Tree(adj_mat, labels)
+
+    # define mutation matrix
+    mutation_matrix = jnp.array([[0, 0, 0, 1, 0], [0, 0, 1, 1, 1], [0, 1, 0, 0, 0]])
+
+    # define error rates
+    alpha = 0.5
+    beta = 0.5
+    theta = (alpha, beta)
+    # should result in a (n)* (n+1) * (m) tensor or all elements 0.5
+
+    # expected
+    # manually compute expected logprob
+    expected = 5 * jnp.log(4 * jnp.exp(3 * jnp.log(0.5)))
+
+    # define logprob fn
+    log_prob = logprob.logprobability_fn(mutation_matrix, tree, theta)
+    assert jnp.equal(log_prob, expected)
+
+
+def test_logprobability_fn_direction():
+    """Test logprobability_fn, test if error in mutation matrix gives worse logprob."""
+    # define tree
+    adj_mat = jnp.array([[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0]])
+    labels = jnp.array([0, 1, 2, 3])
+    tree = Tree(adj_mat, labels)
+
+    # define mutation matrix
+    mutation_matrix_true = jnp.array(
+        [[0, 0, 0, 1, 0], [0, 0, 1, 1, 1], [0, 1, 0, 0, 0]]
+    )
+    mutation_matrix_false = jnp.array(
+        [[0, 0, 0, 0, 1], [1, 1, 1, 0, 0], [0, 0, 1, 0, 0]]
+    )
+
+    # define error rates
+    alpha = 0.1
+    beta = 0.3
+    theta = (alpha, beta)
+
+    # define logprob fn for true mutation matrix
+    log_prob_true = logprob.logprobability_fn(mutation_matrix_true, tree, theta)
+
+    # define logprob fn for false mutation matrix
+    log_prob_false = logprob.logprobability_fn(mutation_matrix_false, tree, theta)
+
+    assert log_prob_true > log_prob_false
