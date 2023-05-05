f17492b58a8f75ba22ed74291595d0103f5e6991
gordonkoehn <gordon@koehn.net>
2023-05-05 13:30:14 +0200

    fix init

--- src/pyggdrasil/serialize/__init__.py
+++ src/pyggdrasil/serialize/__init__.py
@@ -3,13 +3,13 @@ serialization and  deserialization of trees and cohorts."""
 from ._to_json import (
     serialize_tree_to_dict,
     deserialize_tree_from_dict,
-    read_mcmc_sample,
+    read_mcmc_samples,
     save_mcmc_sample,
 )
 
 __all__ = [
     "serialize_tree_to_dict",
     "deserialize_tree_from_dict",
-    "read_mcmc_sample",
+    "read_mcmc_samples",
     "save_mcmc_sample",
 ]
