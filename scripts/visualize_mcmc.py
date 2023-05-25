261eabe4e92c6f822c92e7ac65aa16236931c39b
gordonkoehn <gordon@koehn.net>
2023-05-25 10:34:43 +0200

    WIP script

--- scripts/visualize_mcmc.py
+++ scripts/visualize_mcmc.py
@@ -13,3 +13,54 @@ Example Usage with arguments:
    XXXXXXXXXXXXXXXXXXXXXXX
 
 """
+
+import argparse
+
+
+def create_parser() -> argparse.Namespace:
+    """
+    Parser for required input user.
+
+    Returns:
+        args: argparse.Namespace
+    """
+
+    parser = argparse.ArgumentParser(description="Visualize MCMC samples.")
+
+    parser.add_argument(
+        "--true_tree_fp",
+        required=False,
+        help="Fullpath of true tree if known, else None.",
+        default=None,
+    )
+
+    parser.add_argument(
+        "--out_dir",
+        required=True,
+        help="Output directory to save the MCMC samples.",
+        type=str,
+    )
+
+    parser.add_argument(
+        "--data_fp", required=True, help="File path containing the data.", type=str
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main() -> None:
+    """
+    Main function for the script.
+
+    Returns:
+        None
+    """
+
+    # get arguments
+    create_parser()
+
+
+if __name__ == "__main__":
+    main()
