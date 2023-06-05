33b6ed6e6b3b5c66495dce231f96804498580e43
gordonkoehn <gordon@koehn.net>
2023-06-05 09:13:57 +0200

    adding generate_deep_tree

--- src/pyggdrasil/tree_inference/_tree_generator.py
+++ src/pyggdrasil/tree_inference/_tree_generator.py
@@ -4,10 +4,12 @@ Note: this is part of the private API.
 """
 import jax
 
-import jax.numpy as jnp
+import numpy as np
+
+from pyggdrasil.tree_inference import JAXRandomKey
 
 
-def generate_star_tree(n_nodes: int) -> jax.Array:
+def generate_star_tree(n_nodes: int) -> np.ndarray:
     """Generate a star tree of n_nodes nodes
        Root is the highest index node.
 
@@ -19,7 +21,33 @@ def generate_star_tree(n_nodes: int) -> jax.Array:
         A tree with n_nodes nodes, each node descending from the root.
         I.e. matrix with last row all ones.
     """
-    tree = jnp.zeros((n_nodes, n_nodes), dtype=int)
+    tree = np.zeros((n_nodes, n_nodes), dtype=int)
     tree[-1, :] = 1
+
+    return tree
+
+
+def generate_deep_tree(rng: JAXRandomKey, n_nodes: int) -> np.ndarray:
+    """Generate a deep tree of n_nodes nodes.
+
+    Args:
+        rng: JAX random key.
+        n_nodes: Number of nodes in the tree.
+
+    Returns:
+        Adjacency matrix of a deep tree with n_nodes nodes.
+    """
+    tree = np.zeros((n_nodes, n_nodes), dtype=int)
+
+    # generate a random permutation of the nodes
+    nodes_except_root = n_nodes - 1
+    permutation = jax.random.permutation(rng, nodes_except_root)
+
+    # add edges from root to each node in the permutation
+    root = n_nodes - 1
+    parent = root
+    for child in permutation:
+        tree[parent, child] = 1
+        parent = child
 
     return tree
