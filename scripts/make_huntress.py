d26e08d7997d487620b0b862b510f0ef0b848bed
gordonkoehn <gordon@koehn.net>
2023-06-08 15:00:24 +0200

    open file

---
+++ scripts/make_huntress.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Make a TreeNode tree given a mutation matrix
+to generate a huntress tree.
+
+Example Usage:
+poetry run python ../scripts/make_huntress.py
+
+Example Usage with arguments:
+    poetry run python scripts/plot_trees.py
+    --data_fp data/mutation_matrices/mark00
+    --out_dir data/plots/tree/mark00
+
+"""
