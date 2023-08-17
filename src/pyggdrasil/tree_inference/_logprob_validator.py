807e654001ac2dd5b35427c4157ae775240bd7b3
gordonkoehn <gordon@koehn.net>
2023-08-17 17:36:40 +0200

    unverified validator log probability

--- src/pyggdrasil/tree_inference/_logprob_validator.py
+++ src/pyggdrasil/tree_inference/_logprob_validator.py
@@ -5,6 +5,7 @@ The log-probability functions are used to calculate the log-probability of a tre
 Implements a dumb version of the log-probability function, which is used for testing.
 """
 
+import jax.numpy as jnp
 
 import pyggdrasil.tree_inference._tree as tr
 
@@ -14,7 +15,6 @@ from pyggdrasil.tree_inference import Tree, ErrorRates
 def _expected(
     tree: Tree,
     mutation_i,
-    cell_j,
     cell_attachment,
 ) -> int:
     """Calculates the expected likelihood of a tree given error rates and data.
@@ -75,3 +75,58 @@ def _probability(data: int, expected: int, error_rates: ErrorRates) -> float:
         raise ValueError(
             f"Invalid data and expected values: data={data}, expected={expected}"
         )
+
+
+def _marginalize_attachment(
+    mutation_i, tree: Tree, data, error_rates: ErrorRates
+) -> float:
+    """Marginalize attachment over all possible nodes to attach to."""
+
+    list_of_nodes_to_attach_to = (
+        tree.labels
+    )  # may attach to any nodes even root - though ideally one removes such data
+
+    prob = 0  # prob of tree given a given cell is attached ANYWHERE on given mutation i
+    for node in list_of_nodes_to_attach_to:
+        expected = _expected(tree, mutation_i, node)
+        p_cell_mutation_attachment = _probability(data, expected, error_rates)
+        prob += p_cell_mutation_attachment
+    return prob
+
+
+def _combined_log_prob_cells(
+    mutation_i, tree: Tree, data_cells_per_mutation, error_rates: ErrorRates
+) -> float:
+    """Returns the combined log-probability of all cells for ANY attachment,
+     for a given mutation.
+
+    Args:
+        data_cells_per_mutation: data for all cells for a given mutation,
+         in order of mutations
+    """
+
+    prob = 0
+    for data in data_cells_per_mutation:
+        prob += jnp.log(_marginalize_attachment(mutation_i, tree, data, error_rates))
+
+    return float(prob)
+
+
+def _combine_log_prob_mutations(tree: Tree, data, error_rates: ErrorRates) -> float:
+    """Returns the combined log-probability of all mutations, for ANY attachment.
+
+    Args:
+        data: data for all cells for all mutations, in order of mutations
+    """
+
+    prob = 0
+    for mutation in tree.labels:
+        prob += _combined_log_prob_cells(mutation, tree, data[mutation], error_rates)
+
+    return prob
+
+
+def logprobability_fn_validator(tree: Tree, data, error_rates: ErrorRates) -> float:
+    """Returns the log-probability of a tree given data and error rates."""
+
+    return _combine_log_prob_mutations(tree, data, error_rates)
--- tests/tree_inference/test_logprob_validator.py
+++ tests/tree_inference/test_logprob_validator.py
@@ -1,10 +1,20 @@
 """Tests for the logprob_validator module."""
 
+import pytest
+
 import jax.numpy as jnp
 
+import pyggdrasil as yg
+
+import jax.random as random
 
 from pyggdrasil.tree_inference import Tree
-from pyggdrasil.tree_inference._logprob_validator import _expected
+
+import pyggdrasil.tree_inference._logprob as logprob
+from pyggdrasil.tree_inference._logprob_validator import (
+    _expected,
+    logprobability_fn_validator,
+)
 
 
 def test_expected():
@@ -46,7 +56,72 @@ def test_expected():
     for mutation_i in range(5):  #  forget root
         cell_j = 0
         print("mutation_i", mutation_i)
-        fn_value = _expected(tree, mutation_i, cell_j, cell_attachment).__int__()
+        fn_value = _expected(tree, mutation_i, cell_attachment).__int__()
         expected = expected_mat[mutation_i, cell_j].__int__()
         print("fn_value", fn_value)
         assert fn_value == expected
+
+
+def mutation_data_tree_error(n_cells, n_mutations, error_rates, seed) -> tuple:
+    """Define tree, error settings, and mutation matrix for testing."""
+
+    # make random key jax
+    key = random.PRNGKey(seed)
+    # split into 2 keys
+    key1, key2 = random.split(key)
+    # make random number for tree generation
+    seed_tree = random.randint(key1, (), 0, 1000000)
+
+    # make true tree - random
+    tree = yg.tree_inference.make_tree(
+        n_mutations + 1, yg.tree_inference.TreeType.RANDOM, int(seed_tree)
+    )
+
+    # make mutation matrix
+    params_model = yg.tree_inference.CellSimulationModel(
+        n_cells=n_cells,
+        n_mutations=n_mutations,
+        fpr=error_rates.value.fpr,
+        fnr=error_rates.value.fnr,
+        na_rate=0.0,
+        observe_homozygous=False,
+        strategy=yg.tree_inference.CellAttachmentStrategy.UNIFORM_INCLUDE_ROOT,
+    )
+    data = yg.tree_inference.gen_sim_data(params_model, key2, tree)
+
+    # define error rates
+    error_rate = (error_rates.value.fpr, error_rates.value.fnr)
+
+    return tree, error_rate, jnp.array(data["noisy_mutation_mat"])
+
+
+@pytest.mark.parametrize("n_cells", [3, 4, 5])
+@pytest.mark.parametrize("n_mutations", [2, 3, 4])
+@pytest.mark.parametrize(
+    "error_rates",
+    [
+        yg.tree_inference.ErrorCombinations.IDEAL,
+        yg.tree_inference.ErrorCombinations.TYPICAL,
+        yg.tree_inference.ErrorCombinations.LARGE,
+    ],
+)
+@pytest.mark.parametrize("seed", [23, 890, 2])
+def test_logprobability_fn_against_validator(n_cells, n_mutations, error_rates, seed):
+    """Test logprobability function against validator."""
+
+    # define tree, error rates, and mutation matrix
+    tree, error_rate, data = mutation_data_tree_error(
+        n_cells, n_mutations, error_rates, seed
+    )
+
+    tree = Tree.tree_from_tree_node(tree)
+
+    # run fast logprob
+    logprob_fast = logprob.logprobability_fn(data, tree, error_rate)
+
+    # run slow logprob
+    logprob_validator = logprobability_fn_validator(tree, data, error_rate)
+
+    # assert equal
+    print(f"\nfast: {logprob_fast}\nvalidator: {logprob_validator}")
+    assert jnp.isclose(logprob_fast, logprob_validator, atol=1e-6)
