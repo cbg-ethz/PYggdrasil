Fix Log-Probability Calculation / Tree Node Ordering (#149)
* update gitignore to vs code

* skelleton log prob verify

* adding manual test n3 m2

* add minor docstring to ancesto matrix

* add mutation likelihood test

* numpy sum / exp test failing

* orthogonal log prob without einsum and log sum exp

* adjust docsting

* add Expected

* add P

* unverified validator log probability

* WIP building 2nd literal implementation of log prob slow

* add p

* add log p

* add log_prob_all_mutations

* sum over cells

* log prob final

* untested tests for both log prob

* tests work

* add derivation

* refine validator 1

* remove broken validtor 2

* add noise test

* add more general typing

* add perfect topology tests

* add perfect tree est to logprob fast

* add icloud nosync folder for data

* remove outdated

* add test with extensive logging to be validated manually.

* enhance logging

* found bug in validator exp

* rednaming

* cleanup

* adjust significnace level noise prob

* remove redundant test - up the test params

* add back in test orthogonal

* add test of verify and validator logprobs

* up the cell/mutation numbers  in tests

* improve summing precison

* scale down the tests

* add reorder tree fn on linear algrbra

* reforctor to fast implementation of _reorder_trees

* add OrderedTrees

* force reordering of MCMC kernel

* test that orderd trees perform better on logprob

* fix potential circular import

* order the initial tree

* numpy to jnp switch

* fix shape array jit

src/pyggdrasil/tree_inference/_logprob.py
@@ -9,6 +9,8 @@ from typing import Callable
 
 import pyggdrasil.tree_inference._tree as tr
 from pyggdrasil.tree_inference._tree import Tree
+from pyggdrasil.tree_inference._ordered_tree import OrderedTree
+
 
 from pyggdrasil.tree_inference._interface import (
     MutationMatrix,
@@ -36,7 +38,7 @@ def create_logprob(data: MutationMatrix, rates: ErrorRates) -> Callable:
         logprob_ (Callable): function that calculates the log-probability of a tree
     """
 
-    def logprob_(tree) -> float:
+    def logprob_(tree: OrderedTree) -> float:
         """Calculates the log-probability of a tree given error rates and data.
 
         Args:
@@ -50,12 +52,14 @@ def create_logprob(data: MutationMatrix, rates: ErrorRates) -> Callable:
     return logprob_
 
 
-def logprobability_fn(data: MutationMatrix, tree: tr.Tree, theta: ErrorRates) -> float:
+def logprobability_fn(
+    data: MutationMatrix, tree: OrderedTree, theta: ErrorRates
+) -> float:
     """Calculates the log-probability of a tree given error rates and data.
 
     Args:
         data: observed mutation matrix to calculate the log-probability of
-        tree: tree to calculate the log-probability of
+        tree: tree to calculate the log-probability of, must be OrderedTree
         theta: \theta = (\fpr, \fnr) error rates
 
     Returns:
@@ -171,3 +175,65 @@ def _mutation_likelihood(
     # TODO: implement homozygous mutations / missing data
 
     return mutation_likelihood
+
+
+def _logprobability_fn_verify(
+    data: MutationMatrix, tree: tr.Tree, theta: ErrorRates
+) -> float:
+    """Calculates the log-probability of a tree given error rates and data.
+        Uses basic numpy sums and exp /log to verify einsum /
+         log sum exp implementation.
+    Args:
+        data: observed mutation matrix to calculate the log-probability of
+        tree: tree to calculate the log-probability of
+        theta: \theta = (\fpr, \fnr) error rates
+    Returns:
+        log-probability of the tree
+        Implements the log-probability function from the paper: Equation 13
+        P(D|T,\theta) = \frac{T,\theta | D}{P(T,\theta)}
+    """
+
+    #  get log P(D_{ij} | A(T)_i~~sigma_j) i.e. the log mutation likelihood
+    #         log (P(D_{ij} | A(T)_{i˜sigma_j}) )
+    #             i / axis 0 has n dimensions sum over mutation nodes (n)
+    #                 - each mutation
+    #             j / axis 1 has m dimensions sum over cells (m)
+    #                 - each cell
+    #             k / axis 2 has n+1 dimensions sum over nodes (n+1)
+    #                 - attachment to mutation and root
+    # TODO: replace with _log_mutation_likelihood_verify
+    log_mutation_likelihood = _log_mutation_likelihood(tree, data, theta)
+    print(f"log-mutation likelihood: {log_mutation_likelihood.shape}")
+
+    # verify that the first dimension is the number of mutations
+    assert log_mutation_likelihood[:, 1, 1].shape[0] == tree.labels.shape[0] - 1
+
+    # sum over the n cells  - axis 1 / i
+    carrier = jnp.sum(log_mutation_likelihood, axis=0)
+
+    # verify the dimensions are correct
+    print(f"carrier after sum over mutations: {carrier.shape}")
+    assert carrier.shape == (
+        data.shape[1],
+        tree.tree_topology.shape[0],
+    )  # (m cells, k=n+1 not needed as adjacency included root)
+
+    # exp the carrier
+    carrier = jnp.exp(carrier)
+    # check the dimensions of the attachment axis next to be summed over
+    print(f"carrier after exp: {carrier.shape}")
+    assert carrier.shape[1] == tree.tree_topology.shape[0]
+
+    # sum over the n+1 nodes - axis 2 / k
+    carrier = jnp.sum(carrier, axis=1)  # axis 1 is now k, then (m, )
+
+    # verify the dimensions are correct
+    print(f"carrier after sum over nodes: {carrier.shape}")
+    assert carrier.shape == (data.shape[1],)  # (m, )
+
+    # log the carrier
+    carrier = jnp.log(carrier)
+    # sum over the m cells - axis 0 / j
+    carrier = jnp.sum(carrier, axis=0)  # axis 0 is now j, then (1, )
+
+    return float(carrier)

src/pyggdrasil/tree_inference/_tree.py
@@ -224,6 +224,7 @@ def _get_ancestor_matrix(adj_matrix: Array):
     Returns:
         ancestor_matrix: boolean matrix where the (i,j)
         entry is True if node i is an ancestor of node j.
+        every node is an ancestor of itself.
     """
 
     # ensure is jax array
@@ -267,8 +268,10 @@ def _get_root_label(tree: Tree) -> int:
     return root_label
 
 
-def _reorder_tree(tree: Tree, from_labels, to_labels):
+def _reorder_tree_verify(tree: Tree, to_labels):
     """Reorders tree from current labels to new labels
+
+    Uses loop to reorder tree - SLOW. >100x slower than _reorder_tree.
 
     Args:
         tree: Tree
@@ -282,6 +285,7 @@ def _reorder_tree(tree: Tree, from_labels, to_labels):
 
     """
     size = tree.tree_topology.shape[0]
+    from_labels = tree.labels
     new_adj = jnp.zeros((size, size))
     for row in range(size):
         prior__row_label = from_labels[row]
@@ -294,6 +298,31 @@ def _reorder_tree(tree: Tree, from_labels, to_labels):
 
     reordered_tree = Tree(tree_topology=new_adj, labels=to_labels)
     return reordered_tree
+
+
+def _reorder_tree(tree: Tree, new_labels: Array) -> Tree:
+    """Reorders tree from current labels to new labels,
+    using linear algebra.
+
+    Args:
+        tree: Tree
+            tree to reorder
+        new_labels: Array
+            new labels of tree to be returned
+    Returns:
+        reordered_tree: Tree
+    """
+    # unpack tree
+    adj_matrix = tree.tree_topology
+    labels = tree.labels
+    # Create a permutation matrix based on the mapping
+    P = jnp.zeros((len(labels), len(labels)), dtype=jnp.int32)
+    matching_indices = jnp.where(labels[:, None] == new_labels, size=labels.shape[0])
+    P = P.at[matching_indices[0], matching_indices[1]].set(1)
+    # Reorder the adjacency matrix using matrix multiplication
+    reordered_adj_matrix = P.T @ adj_matrix @ P
+    # make and return tree
+    return Tree(reordered_adj_matrix, new_labels)
 
 
 def is_same_tree(tree1: Tree, tree2: Tree) -> bool:
@@ -313,9 +342,7 @@ def is_same_tree(tree1: Tree, tree2: Tree) -> bool:
 
     # if the trees are not the same, check if their labels are just ordered differently
     if result is False:
-        tree2_reordered = _reorder_tree(
-            tree2, from_labels=tree2.labels, to_labels=tree1.labels
-        )
+        tree2_reordered = _reorder_tree(tree2, tree1.labels)
         result = jnp.all(
             tree1.tree_topology == tree2_reordered.tree_topology
         ) and jnp.all(tree1.labels == tree2_reordered.labels)

tests/tree_inference/test_logprob.py
@@ -2,9 +2,11 @@
 # _logprob.py
 
 import jax.numpy as jnp
+import jax.random as random
 import pytest
 
 import pyggdrasil.tree_inference._logprob as logprob
+import pyggdrasil as yg
 from pyggdrasil.tree_inference._tree import Tree
 import pyggdrasil.tree_inference._tree as tr
 
@@ -161,7 +163,7 @@ def test_logprobability_fn():
     expected = 5 * jnp.log(4 * jnp.exp(3 * jnp.log(0.5)))
 
     # define logprob fn
-    log_prob = logprob.logprobability_fn(mutation_matrix, tree, theta)
+    log_prob = logprob.logprobability_fn(mutation_matrix, tree, theta)  # type: ignore
     assert jnp.equal(log_prob, expected)
 
 
@@ -186,9 +188,257 @@ def test_logprobability_fn_direction():
     theta = (alpha, beta)
 
     # define logprob fn for true mutation matrix
-    log_prob_true = logprob.logprobability_fn(mutation_matrix_true, tree, theta)
+    log_prob_true = logprob.logprobability_fn(
+        mutation_matrix_true, tree, theta  # type: ignore
+    )
 
     # define logprob fn for false mutation matrix
-    log_prob_false = logprob.logprobability_fn(mutation_matrix_false, tree, theta)
+    log_prob_false = logprob.logprobability_fn(
+        mutation_matrix_false, tree, theta  # type: ignore
+    )
 
     assert log_prob_true > log_prob_false
+
+
+def test_logprobability_fn_exact_m2n3():
+    """Test logprobability_fn manually.
+
+    Manual Logprob calculation for a tree with 2 mutations and 3 cells.
+    See https://github.com/cbg-ethz/PYggdrasil/pull/149 for calculation.
+
+    """
+    # define tree
+    adj_mat = jnp.array([[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0]])
+    labels = jnp.array([0, 1, 2, 3])
+    tree = Tree(adj_mat, labels)
+
+    # define mutation matrix
+    mutation_matrix = jnp.array([[0, 1], [1, 1], [0, 0]])
+
+    # define error rates
+    alpha = 0.1
+    beta = 0.3
+    theta = (alpha, beta)
+
+    # expected
+    # manually compute expected logprob
+    # expected = -0.79861
+    expected = jnp.log(
+        0.3 * 0.7 * 0.9 + 0.9 * 0.7 * 0.9 + 0.9 * 0.1 * 0.3 + 0.9 * 0.1 * 0.9
+    ) + jnp.log(0.7 * 0.7 * 0.9 + 0.1 * 0.7 * 0.9 + 0.1 * 0.1 * 0.3 + 0.1 * 0.1 * 0.9)
+
+    # define logprob fn
+    log_prob = logprob.logprobability_fn(mutation_matrix, tree, theta)  # type: ignore
+    assert jnp.isclose(log_prob, expected, atol=1e-4)
+
+
+def test_mutation_likelihood_fn_exact_m2n3():
+    """Test mutation_likelihood_fn manually.
+
+    Manual mutation likelihood calculation for a tree with 2 mutations and 3 cells.
+    See https://github.com/cbg-ethz/PYggdrasil/pull/149 for calculation.
+    """
+
+    # define tree
+    adj_mat = jnp.array([[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0]])
+    labels = jnp.array([0, 1, 2, 3])
+    tree = Tree(adj_mat, labels)
+
+    # get ancestor matrix
+    ancestor_mat = tr._get_ancestor_matrix(tree.tree_topology)
+
+    # define mutation matrix
+    mutation_mat = jnp.array([[0, 1], [1, 1], [0, 0]])
+
+    # define error rates
+    alpha = 0.1
+    beta = 0.3
+    theta = (alpha, beta)
+
+    # get mutation likelihood
+    mutation_likelihood = logprob._mutation_likelihood(
+        mutation_mat, ancestor_mat, theta
+    )
+
+    # expected
+    # manually compute expected mutation likelihood
+    expected = jnp.array(
+        [
+            # n / i = 0
+            [
+                # m / j = 0
+                [0.3, 0.9, 0.9, 0.9],
+                # m / j = 1
+                [0.7, 0.1, 0.1, 0.1],
+            ],
+            # n / i = 1
+            [
+                # m / j = 0
+                [0.7, 0.7, 0.1, 0.1],
+                # m / j = 1
+                [0.7, 0.7, 0.1, 0.1],
+            ],
+            # n / i = 2
+            [
+                # m / j = 0
+                [0.9, 0.9, 0.3, 0.9],
+                # m / j = 1
+                [0.9, 0.9, 0.3, 0.9],
+            ],
+        ]
+    )
+
+    assert jnp.isclose(mutation_likelihood, expected, atol=1e-10).all()
+
+
+def mutation_data_tree_error(
+    n_cells,
+    n_mutations,
+    error_rates: yg.tree_inference.ErrorCombinations,
+    seed,
+) -> tuple:
+    """Define tree, error settings, and mutation matrix for testing."""
+
+    # make random key jax
+    key = random.PRNGKey(seed)
+    # split into 2 keys
+    key1, key2 = random.split(key)
+    # make random number for tree generation
+    seed_tree = random.randint(key1, (), 0, 1000000)
+
+    # make true tree - random
+    tree = yg.tree_inference.make_tree(
+        n_mutations + 1, yg.tree_inference.TreeType.RANDOM, int(seed_tree)
+    )
+
+    # make mutation matrix
+    params_model = yg.tree_inference.CellSimulationModel(
+        n_cells=n_cells,
+        n_mutations=n_mutations,
+        fpr=error_rates.value.fpr,
+        fnr=error_rates.value.fnr,
+        na_rate=0.0,
+        observe_homozygous=False,
+        strategy=yg.tree_inference.CellAttachmentStrategy.UNIFORM_INCLUDE_ROOT,
+    )
+    data = yg.tree_inference.gen_sim_data(params_model, key2, tree)
+
+    # define error rates
+    error_rate = (error_rates.value.fpr, error_rates.value.fnr)
+
+    return (
+        tree,
+        error_rate,
+        jnp.array(data["noisy_mutation_mat"]),
+        jnp.array(data["perfect_mutation_mat"]),
+    )
+
+
+@pytest.mark.parametrize("n_cells", [3, 5, 50])
+@pytest.mark.parametrize("n_mutations", [2, 4, 15])
+@pytest.mark.parametrize(
+    "error_rates",
+    [
+        yg.tree_inference.ErrorCombinations.IDEAL,
+        yg.tree_inference.ErrorCombinations.TYPICAL,
+        yg.tree_inference.ErrorCombinations.LARGE,
+    ],
+)
+@pytest.mark.parametrize("seed", [23, 890])
+def test_orthogonal_log_probs(n_cells, n_mutations, error_rates, seed):
+    """Test orthogonal_log_prob implementations
+
+    Fast: logprob._logprobability_fn: uses einsum and logsumexp
+    Slow: logprob._logprobability_fn_verify : uses jnp.sum
+    """
+
+    # define tree, error rates, and mutation matrix
+    tree, error_rate, data, _ = mutation_data_tree_error(
+        n_cells, n_mutations, error_rates, seed
+    )
+
+    # convert tree: TreeNode -> Tree
+    tree = yg.tree_inference.Tree.tree_from_tree_node(tree)
+
+    # run fast logprob
+    logprob_fast = logprob.logprobability_fn(data, tree, error_rate)  # type: ignore
+
+    # run slow / no einsum logsumexp logprob
+    logprob_slow = logprob._logprobability_fn_verify(data, tree, error_rate)
+
+    # assert equal
+    assert jnp.isclose(logprob_fast, logprob_slow, atol=1e-4)
+
+
+@pytest.mark.parametrize("n_cells", [40])
+@pytest.mark.parametrize("n_mutations", [2, 4])
+@pytest.mark.parametrize("seed", [23, 5])
+def test_logprob_no_noise_many_cells_wrong_tree(
+    n_cells: int, n_mutations: int, seed: int
+):
+    """Test if given no noise and many cell small trees can be identified.
+    make a small true tree, and compare its likelihood to that of a mcmc 1 tree."""
+
+    error_rate = yg.tree_inference.ErrorCombinations.IDEAL
+
+    tree_n, error_rate, _, data = mutation_data_tree_error(
+        n_cells, n_mutations, error_rate, seed
+    )
+
+    tree = Tree.tree_from_tree_node(tree_n)
+
+    rng = random.PRNGKey(seed)
+
+    # until we found a different tree
+    tree_mcmc01_nd = yg.tree_inference.evolve_tree_mcmc(tree_n, 1, rng)
+    while yg.compare_trees(tree_n, tree_mcmc01_nd):  # type: ignore
+        rng, _ = random.split(rng)
+        tree_mcmc01_nd = yg.tree_inference.evolve_tree_mcmc(tree_n, 1, rng)
+
+    tree_mcmc01 = Tree.tree_from_tree_node(tree_mcmc01_nd)
+
+    # run logprob on true tree
+    logprob_true_tree = logprob.logprobability_fn(
+        data, tree, error_rate  # type: ignore
+    )
+
+    # run logprob on mcmc tree
+    logprob_mcmc_tree = logprob.logprobability_fn(
+        data, tree_mcmc01, error_rate  # type: ignore
+    )
+
+    print(f"\ntrue tree:\n {tree_n}\nmcmc tree:\n {tree_mcmc01_nd}")
+    print(f"\nIs the same tree: " f"{yg.compare_trees(tree_n, tree_mcmc01_nd)}")
+    print(f"\ntrue tree: {logprob_true_tree}\nmcmc tree: {logprob_mcmc_tree}")
+    assert logprob_true_tree >= logprob_mcmc_tree
+
+
+@pytest.mark.parametrize("n_cells", [10])
+@pytest.mark.parametrize("n_mutations", [3, 4])
+@pytest.mark.parametrize("seed", [23, 45])
+def test_log_prob_unordered_tree(n_cells: int, n_mutations: int, seed: int):
+    """Try to calculate the log-prob for an unordered tree."""
+
+    error_rate = yg.tree_inference.ErrorCombinations.IDEAL
+
+    tree_n, error_rate, _, data = mutation_data_tree_error(
+        n_cells, n_mutations, error_rate, seed
+    )
+
+    tree = Tree.tree_from_tree_node(tree_n)
+
+    # make unordered tree
+    # make random permutation of labels
+    rng = random.PRNGKey(seed)
+    labels_random = random.permutation(rng, tree.labels)
+    tree_unordered = tr._reorder_tree(tree, labels_random)
+
+    logprob_unordered = logprob.logprobability_fn(
+        data, tree_unordered, error_rate  # type: ignore
+    )
+
+    logprob_ordered = logprob.logprobability_fn(data, tree, error_rate)  # type: ignore
+
+    print(f"\nunordered tree:\n {logprob_unordered}\nordered tree:\n {logprob_ordered}")
+
+    assert logprob_unordered < logprob_ordered